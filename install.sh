4e2e77f6b804100a68012792d131c8f9245f8d61
Funk <mfunk@Mikes-Mac.local>
2014-01-18 13:48:24 -0800

    fixed syntax error

--- install.sh
+++ install.sh
@@ -61,9 +61,9 @@ if [ ${ROOT_ONLY} -gt 0 ] && [ ${USER} != "root" ]; then
 fi
 
 # Tools.
-type grep >/dev/null 2>&1 || { echo >&2 "Please install 'grep'";; exit 1; }
-type sed >/dev/null 2>&1 || { echo >&2 "Please install 'sed'";; exit 1; }
-type date >/dev/null 2>&1 || { echo >&2 "Please install 'date'";; exit 1; }
+type grep >/dev/null 2>&1 || { echo >&2 "Please install 'grep'"; exit 1; }
+type sed >/dev/null 2>&1 || { echo >&2 "Please install 'sed'"; exit 1; }
+type date >/dev/null 2>&1 || { echo >&2 "Please install 'date'"; exit 1; }
 
 ############################## Initial-touch-all ##############################
 
